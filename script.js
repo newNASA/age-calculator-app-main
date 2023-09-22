5d6913c08b502a1d13ac20b67248c07b0e41c5d1
newNASA <azizbekgulomovweb@gmail>
2023-09-22 22:50:34 +0500

    jfdlskjflalfdjl;kaj

--- script.js
+++ script.js
@@ -16,77 +16,110 @@ function buttonClicked() {
     let dayValue = parseInt(day.value);
     let monthValue = parseInt(month.value) - 1;
     let yearValue = parseInt(year.value);
-    console.log(monthValue);
     const check_month = [3, 5, 8, 10]
 
-    if (!day.value) {
-        day.style.border = "1px solid var(--error)";
-        label__day.style.color = "var(--error)";
-        dayspan.style.color = "var(--error)";
-        dayspan.textContent = "This field is required"
-    } else if (check_month.includes(monthValue) && dayValue >= 31 || monthValue == 1 && dayValue > 28) {
-        dayspan.textContent = "Must be a valid date"
+    if (!day.value || !month.value || !yearValue) {
+        if (!day.value) {
+            day.style.border = "1px solid var(--error)";
+            label__day.style.color = "var(--error)";
+            dayspan.style.color = "var(--error)";
+            dayspan.textContent = "This field is required"
+        } else {
+            day.style.border = "1px solid black";
+            dayspan.style.color = "initial";
+            label__day.style.color = "var(--label-color)";
+            dayspan.textContent = ""
+        }
 
-        year.style.border = "1px solid var(--error)";
-        label__year.style.color = "var(--error)";
-        yearspan.style.color = "var(--error)";
-        month.style.border = "1px solid var(--error)";
-        label__month.style.color = "var(--error)";
-        monthspan.style.color = "var(--error)";
-        day.style.border = "1px solid var(--error)";
-        label__day.style.color = "var(--error)";
-        dayspan.style.color = "var(--error)";
-        return
-    }else {
-        day.style.border = "1px solid black";
-        dayspan.style.color = "initial";
-        label__day.style.color = "var(--label-color)";
-        dayspan.textContent = ""
-    }
+        if (!month.value) {
+            month.style.border = "1px solid var(--error)";
+            label__month.style.color = "var(--error)";
+            monthspan.style.color = "var(--error)";
+            monthspan.textContent = "This field is required";
+        } else {
+            month.style.border = "1px solid black";
+            monthspan.style.color = "initial";
+            label__month.style.color = "var(--label-color)";
+            monthspan.textContent = ""
+        }
 
-    if (!month.value) {
-        month.style.border = "1px solid var(--error)";
-        label__month.style.color = "var(--error)";
-        monthspan.style.color = "var(--error)";
-        monthspan.textContent = "This field is required";
-    } else {
-        month.style.border = "1px solid black";
-        monthspan.style.color = "initial";
-        label__month.style.color = "var(--label-color)";
-        monthspan.textContent = ""
-    }
-
-    if (!year.value) {
-        year.style.border = "1px solid var(--error)";
-        label__year.style.color = "var(--error)";
-        yearspan.style.color = "var(--error)";
-        yearspan.textContent = "This field is required";
-    } else {
+        if (!year.value) {
+            year.style.border = "1px solid var(--error)";
+            label__year.style.color = "var(--error)";
+            yearspan.style.color = "var(--error)";
+            yearspan.textContent = "This field is required";
+        } else {
+            year.style.border = "1px solid black";
+            yearspan.style.color = "initial";
+            label__year.style.color = "var(--label-color)";
+            yearspan.textContent = ""
+        }
+    }else if(day.value && month.value && year.value) {
         year.style.border = "1px solid black";
         yearspan.style.color = "initial";
         label__year.style.color = "var(--label-color)";
         yearspan.textContent = ""
+        month.style.border = "1px solid black";
+        monthspan.style.color = "initial";
+        label__month.style.color = "var(--label-color)";
+        monthspan.textContent = ""
+        day.style.border = "1px solid black";
+        dayspan.style.color = "initial";
+        label__day.style.color = "var(--label-color)";
+        dayspan.textContent = ""
+        if (check_month.includes(monthValue) && dayValue == 31 || monthValue == 1 && dayValue >= 29) {
+            dayspan.textContent = "Must be a valid date"    
+            year.style.border = "1px solid var(--error)";
+            label__year.style.color = "var(--error)";
+            yearspan.style.color = "var(--error)";
+            month.style.border = "1px solid var(--error)";
+            label__month.style.color = "var(--error)";
+            monthspan.style.color = "var(--error)";
+            day.style.border = "1px solid var(--error)";
+            label__day.style.color = "var(--error)";
+            dayspan.style.color = "var(--error)";
+        }else if (!check_month.includes(monthValue) && dayValue > 31) {
+            dayspan.textContent = "Must be valid day"
+            day.style.border = "1px solid var(--error)";
+            label__day.style.color = "var(--error)";
+            dayspan.style.color = "var(--error)";
+        }else{
+            day.style.border = "1px solid black";
+            dayspan.style.color = "initial";
+            label__day.style.color = "var(--label-color)";
+            dayspan.textContent = ""
+        }
+        if(monthValue > 12){
+            monthspan.textContent = "Must be valid month"
+            month.style.border = "1px solid var(--error)";
+            label__month.style.color = "var(--error)";
+            monthspan.style.color = "var(--error)";
+        }else{
+            month.style.border = "1px solid black";
+            monthspan.style.color = "initial";
+            label__month.style.color = "var(--label-color)";
+            monthspan.textContent = ""
+        }
+    }else{
+        const formattedDay = dayValue < 10 ? `0${dayValue}` : dayValue;
+        const formattedMonth = (monthValue + 1) < 10 ? `0${monthValue + 1}` : monthValue + 1;
+    
+        const currentDate = new Date();
+        const enteredDate = new Date(`${yearValue}-${formattedMonth}-${formattedDay}`);
+    
+        const timeDifferenceInMilliseconds = currentDate - enteredDate;
+    
+        const millisecondsInADay = 1000 * 60 * 60 * 24;
+        const daysDifference = Math.floor(timeDifferenceInMilliseconds / millisecondsInADay);
+    
+        const yearsDifference = Math.floor(daysDifference / 365);
+        const monthsDifference = Math.floor((daysDifference % 365) / 30);
+        const remainingDays = daysDifference % 30;
+    
+        years.textContent = yearsDifference;
+        months.textContent = monthsDifference;
+        days.textContent = remainingDays;
     }
-
-    const formattedDay = dayValue < 10 ? `0${dayValue}` : dayValue;
-    const formattedMonth = (monthValue + 1) < 10 ? `0${monthValue + 1}` : monthValue + 1;
-
-    const currentDate = new Date();
-    const enteredDate = new Date(`${yearValue}-${formattedMonth}-${formattedDay}`);
-
-    const timeDifferenceInMilliseconds = currentDate - enteredDate;
-
-    const millisecondsInADay = 1000 * 60 * 60 * 24;
-    const daysDifference = Math.floor(timeDifferenceInMilliseconds / millisecondsInADay);
-
-    const yearsDifference = Math.floor(daysDifference / 365);
-    const monthsDifference = Math.floor((daysDifference % 365) / 30);
-    const remainingDays = daysDifference % 30;
-
-    years.textContent = yearsDifference;
-    months.textContent = monthsDifference;
-    days.textContent = remainingDays;
 }
 
 button.addEventListener('click', buttonClicked);
-
